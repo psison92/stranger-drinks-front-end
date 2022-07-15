85bdc8cf009f251a16d742e747e8fc0633c3aa4c
CoolRy <that007guy@hotmail.com>
2022-07-15 15:39:00 -0400

    made stranger drinks background a component

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,7 @@ const App = () => {
   return (
     <>
       <NavBar user={user} handleLogout={handleLogout} />
+      <StrangerDrinks />
       <Routes>
         <Route
           path="/signup"
