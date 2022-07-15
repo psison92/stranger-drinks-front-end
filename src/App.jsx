2f06f9781ca27897ec7fcdba3ce0c51898c39c51
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-15 08:19:57 -0600

    adds routes and functionality for drinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+
 import NavBar from './components/NavBar/NavBar'
 import Signup from './pages/Signup/Signup'
 import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
+import CreateDrink from './pages/CreateDrink/CreateDrink'
+
 import * as authService from './services/authService'
+import * as drinkService from './services/drinkService'
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
+  const [drinks, setDrinks] = useState([])
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchAllDrinks = async () => {
+      const drinkData = await drinkService.getAll()
+      setDrinks(drinkData)
+    }
+    fetchAllDrinks()
+  }, [])
 
   const handleLogout = () => {
     authService.logout()
@@ -20,6 +33,21 @@ const App = () => {
 
   const handleSignupOrLogin = () => {
     setUser(authService.getUser())
+  }
+
+  const handleAddDrink = async (newDrinkData, photo) => {
+    const newDrink = await drinkService.create(newDrinkData)
+    if (photo) {
+      newDrink.photo = await drinkPhotoHelper(photo, newDrink._id)
+    }
+    setDrinks([...drinks, newDrink])
+    navigate('/') // FIXME Where would we like the user to go after creating a drink
+  }
+
+  const drinkPhotoHelper = async (photo, id) => {
+    const photoData = new FormData()
+    photoData.append('photo', photo) // allows for sending files
+    return await drinkService.addPhoto(photoData, id)
   }
 
   return (
@@ -48,6 +76,10 @@ const App = () => {
               <Navigate to="/login" />
             )
           }
+        />
+        <Route
+          path="/mixDrink"
+          element={user ? <CreateDrink /> : <Navigate to="/login" />}
         />
       </Routes>
     </>
