a27a57ca49ea4f2c97c00eb10fcef0a749c4aca9
CoolRy <that007guy@hotmail.com>
2022-07-16 15:33:59 -0400

    made drink details button and route

--- src/components/DrinkCard/DrinkCard.jsx
+++ src/components/DrinkCard/DrinkCard.jsx
@@ -23,6 +23,11 @@ const DrinkCard = ({drink, user, handleDeleteDrink}) => {
           >
             Delete
           </button>
+          <Link
+            to={`/drink/${drink._id}`}
+            className='btn btn-sm btn-info'
+            > Details
+          </Link>
         </div>
       }
     </div>
