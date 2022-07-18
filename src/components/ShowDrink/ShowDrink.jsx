dada333b9dbeafb9c1aaf36942bc67a2d301d54c
CoolRy <that007guy@hotmail.com>
2022-07-18 11:13:58 -0400

    add ingredient name

--- src/components/ShowDrink/ShowDrink.jsx
+++ src/components/ShowDrink/ShowDrink.jsx
@@ -3,7 +3,7 @@ const ShowDrink = (props) => {
     <>
     <div className="card">
       <div className="card-body">
-        <h5 className="card-text">{props.rec.ingredient}</h5>
+        <h5 className="card-text">{props.rec.ingredient.name}</h5>
         <h5 className="card-text">{props.rec.quantity}</h5>
         <h5 className="card-text">{props.rec.unit}</h5>
       </div>
